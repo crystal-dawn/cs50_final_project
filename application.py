8624d125b6a9899cc07d313d64849f1eb758013f
Crystal Yungwirth <crystalyungwirth@gmail.com>
2020-12-06 18:13:46 +0000

    FEAT: Register form

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from cs50 import SQL
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Configure Application
 # First argument is the name of the application's module or package,
@@ -9,10 +9,21 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Identify which URL should trigger functiongit remote add origin https://github.com/crystal-dawn/cs50_final_project.git
+# Configure CS50 to use SQLite database
+db = SQL("sqlite:///youtine.db")
+
+# Identify which URL should trigger function it remote add origin https://github.com/crystal-dawn/cs50_final_project.git
 @app.route('/')
 # Function is given name, used to generate URLs for a particular function
 def login():
     # render the main page
-    return render_template("login.html")
+    return render_template("login.html", show="show")
 
+# Registration form
+# @app.route('/', methods=["POST"])
+# def register():
+#     """ Register User """
+
+#     if request.method == "POST":
+#         username = request.form.get("registerUsername")
+#         existingUsers = db.execute("SELECT * FROM users WHERE username = :username", username=username)
