8522d78063d3e13b6150f49a3d18e1fbc7347232
Crystal Yungwirth <crystalyungwirth@gmail.com>
2020-12-19 19:40:07 +0000

    feat: Message Flashing

--- application.py
+++ application.py
@@ -1,13 +1,31 @@
 from cs50 import SQL
-from flask import Flask, render_template, request, redirect
+from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_session import Session
+from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 # Configure Application
 # First argument is the name of the application's module or package,
 # A single module uses _name_ so Flask knows where to look for templates/static files
 app = Flask(__name__)
 
+### From CS50 Finance starter code 2020 ###
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Configure CS50 to use SQLite database
 db = SQL("sqlite:///youtine.db")
@@ -29,21 +47,30 @@ def register():
 
     # User reached route via POST (as by submitting a form via POST)
     else:
-        password_is_valid = validate_password()
-        # TODO: Replace 'register.html' with account page if there are no errors
-        return validate_username() or validate_password() or render_template('register.html', error = None)
+        is_password_valid = validate_password()
+        is_username_valid = validate_username()
+
+        if is_username_valid:
+            flash(is_username_valid, 'error')
+        if is_password_valid:
+            flash(is_password_valid, 'error')
+        else:
+            return redirect(url_for('register'))
+
+        return render_template('register.html')
 
 # Validate username
 def validate_username():
     username = request.form.get("registerUsername")
+    # Select existing users from database
     users = db.execute("SELECT * FROM users WHERE username = :username", username=username)
 
     # Username validation
     if not username or not username.strip():
-        return render_template('register.html', username_error = 'Name is missing')
+        return 'Name is missing'
     # Username already exists
     elif len(users):
-        return render_template('register.html', username_error = 'Username unavailable')
+        return 'Username unavailable'
 
 
 # Validate password
@@ -55,5 +82,5 @@ def validate_password():
     app.logger.info("confirmation: %s", password_confirmed)
 
     if not password:
-        return render_template('register.html', password_error = "Choose an eight character password")
-    return render_template('register.html', error = None)
+        return "Choose an eight character password"
+    return None
